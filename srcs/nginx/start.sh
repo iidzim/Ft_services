e4033fa59899f4bbb41c2093c5de9780795e13c6
Ikram Idzim <iidzim@e1r10p10.1337.ma>
2021-02-10 18:46:07 +0100

    nginx fixed

---
+++ srcs/nginx/start.sh
@@ -0,0 +1,10 @@
+echo -e "\n\n\n\n\n\n\n\n" > input
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout /etc/ssl/nginx_cert.key -out /etc/ssl/nginx_cert.crt < input
+rm input
+rc-status
+rc-service nginx restart
+echo -e "1234\n1234\n" > input
+adduser ikram < input
+rm input
+rc-service sshd restart
+/bin/sh
