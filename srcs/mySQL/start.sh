55578e10bbe28f5c39e4443247cdb7e77b39554d
Ikram Idzim <iidzim@e1r11p12.1337.ma>
2021-02-13 19:13:36 +0100

    phpadmin

--- srcs/mySQL/start.sh
+++ srcs/mySQL/start.sh
@@ -1,7 +1,10 @@
 rc-status
+touch /run/openrc/softlevel
+/etc/init.d/mariadb setup
 rc-service mariadb restart
 mysql -e "CREATE USER 'ikram'@'%' IDENTIFIED BY '1234';" 
-mysql -e "CREATE DATABASE wordpress;"
+mysql -e "CREATE DATABASE db;"
 mysql -e "FLUSH PRIVILEGES;"
-mysql -e "GRANT ALL  PRIVILEGES ON *.* TO 'ikram'%'%';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'ikram'@'%';"
+rc-service mariadb restart
 /bin/sh
