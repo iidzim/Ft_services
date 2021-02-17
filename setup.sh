aaeb993a23950514535a149edcb495b047132f40
Ikram Idzim <iidzim@e1r11p12.1337.ma>
2021-02-17 12:01:44 +0100

    metallb

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 minikube start 
 eval $(minikube docker-env)
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 docker build -t nginx ./srcs/nginx/
 docker build -t phpmyadmin ./srcs/phpmyadmin/
 docker build -t wordpress ./srcs/wordpress/
