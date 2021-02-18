994b49eaf652cb58d181a3ace3a39fd0bdc9fa4c
Ikram Idzim <iidzim@e1r11p12.1337.ma>
2021-02-18 12:48:29 +0100

    telegraf

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ docker build -t nginx ./srcs/nginx/
 docker build -t phpmyadmin ./srcs/phpmyadmin/
 docker build -t wordpress ./srcs/wordpress/
 docker build -t mysql ./srcs/mysql/
+docker build -t influxdb ./srcs/influxdb/
 
 kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
