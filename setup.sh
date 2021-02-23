1411be485ae06f3dd1172ef2cc6eb878f8041258
Ikram Idzim <iidzim@e1r11p12.1337.ma>
2021-02-23 12:47:54 +0100

    ftps

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
+export MINIKUBE_HOME=/goinfre/iidzim
 minikube delete
-minikube start 
+minikube start --cpus 4 --memory 4000
 eval $(minikube docker-env)
-
+minikube addons enable metallb
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
@@ -10,7 +11,7 @@ docker build -t nginx ./srcs/nginx/
 docker build -t phpmyadmin ./srcs/phpmyadmin/
 docker build -t wordpress ./srcs/wordpress/
 docker build -t mysql ./srcs/mysql/
-docker build -t influxDB ./srcs/influxdb/
+docker build -t influxdb ./srcs/influxdb/
 docker build -t grafana ./srcs/grafana/
 docker build -t ftps ./srcs/ftps/
 
