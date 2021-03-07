f34c1df7f5c1417f6409425497bad20111b213f7
iidzim9 <52450700+iidzim9@users.noreply.github.com>
2021-03-07 18:58:16 +0100

    ftps

--- srcs/FTPS/start.sh
+++ srcs/FTPS/start.sh
@@ -1,5 +1,12 @@
+echo -e "\n\n\n\n\n\n\n\n" > input
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 365 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem < input
+rm input
 rc-status
+echo -e "1234\n1234\n" > input
+adduser ikram < input
+rm input
 rc-service vsftpd restart
 /usr/bin/telegraf &
 # /bin/sh
 sleep infinity
+
--- srcs/nginx/start.sh
+++ srcs/nginx/start.sh
@@ -1,5 +1,5 @@
 echo -e "\n\n\n\n\n\n\n\n" > input
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout /etc/ssl/nginx_cert.key -out /etc/ssl/nginx_cert.crt < input
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 365 -keyout /etc/ssl/nginx_cert.key -out /etc/ssl/nginx_cert.crt < input
 rm input
 rc-status
 rc-service nginx restart
